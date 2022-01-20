3f108b999efdaa2d6a4db7d739440113ffd7d7da
Yusuke <theshoreunder@gmail.com>
2022-01-20 23:34:55 +0900

    Strategyパターン

---
+++ Strategy/Main/Include/Hand.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file Hand.hpp
+ * 
+ * Handクラス
+ * じゃんけんの手を表すクラス
+ * @author bigface / created on 2022/01/20
+ */
+
+#ifndef HAND_H
+#define HAND_H
+
+#include <string>
+
+class Hand
+{
+public:
+  static const int HANDVALUE_GUU = 0;
+  static const int HANDVALUE_CHO = 1;
+  static const int HANDVALUE_PAA = 2;
+  static const Hand hand[3];
+
+  static Hand getHand(int handvalue);
+  bool isStrongerThan(Hand h);
+  bool isWeakerThan(Hand h);
+  std::string toString();
+
+private:
+  static const std::string name[3];
+  int handvalue;
+
+  Hand(int handvalue);
+  int fight(Hand h);
+};
+
+#endif
---
+++ Strategy/Main/answer.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file answer.cpp
+ * Strategyパターン
+ * 
+ * @author bigface / created on 2022/01/20
+ */
+#include <stdio.h>
+#include <iostream>
+
+int main( int argc, char *argv[] )
+{
+    
+}
